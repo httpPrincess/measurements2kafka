571cc00f76e839088c1b0c21c2c36031aedeb7e2
httpprincess <j.rybicki@fz-juelich.de>
2017-08-28 10:16:57 +0200

    working version

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,29 +1,28 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-from math import sqrt
-from random import uniform, random
+from flask_bootstrap import Bootstrap
+import sys
+
+sys.path.append('../')
+from model.eventstore import store_events, find_events
+from model import Event, ORIGIN, generate_timestamp, generate_event, apply_events
 
 app = Flask(__name__)
+Bootstrap(app)
 
-
-def next_step(x0, dt, stepnbr):
-    return x0 + (0.5 - random()) * sqrt(dt) * uniform(0.0, stepnbr)
-
-
-outx = list()
-outy = list()
+ENTITY_ID = 666
 
 
 @app.before_first_request
 def setup():
-    outx.append(1)
-    outy.append(5)
+    store_single_event(ORIGIN)
 
 
 def update_values(x, y):
-    global outx, outy
-    if x in outx:
-        ind = outx.index(x)
-        outy[ind] = y
+    store_single_event(Event('jj', generate_timestamp(), x, y))
+
+
+def store_single_event(event):
+    store_events(ENTITY_ID, [event])
 
 
 @app.route('/input/', methods=['POST'])
@@ -36,14 +35,29 @@ def put_data():
     return redirect(url_for('index'))
 
 
+def get_times():
+    past_events = find_events(ENTITY_ID)
+    return [e.ts for e in past_events]
+
+
 @app.route('/data/', methods=['GET'])
-def get_data():
-    x = outx[-1]
-    outx.append(x + 1)
-    outy.append(next_step(x, 0.5, len(outx)))
-    return jsonify({
-        'x': outx,
-        'y': outy})
+@app.route('/data/<int:ts>', methods=['GET'])
+def get_data(ts=0):
+    past_events = find_events(ENTITY_ID)
+    # if ts available filter
+    if ts != 0:
+        past_events = filter(lambda a: a.ts < ts, past_events)
+
+    print('Events so far: %r' % past_events)
+    entity = apply_events(past_events)
+
+    # just for fun, by each retrieval we add a new Event
+    if ts == 0:
+        new_event = generate_event(past_events[-1])
+        store_single_event(new_event)
+
+    # transformation for plotting
+    return jsonify({'x': [x for x in entity.keys()], 'y': [y for x, y in entity.items()], 'times': get_times()})
 
 
 @app.route('/', methods=['GET'])
